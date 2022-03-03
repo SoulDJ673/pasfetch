fddeb0cd2db68eb4016f70e8aa314c8c9a754a7e
Drunken Alcoholic <drunkenalcoholic@protonmail.com>
2022-03-03 14:34:53 +0000

    Upload New File

---
+++ uPasfetchAscii.pas
@@ -0,0 +1,46 @@
+unit uPasfetchAscii;
+{$mode objfpc}{$H+}
+
+interface
+
+uses 
+ Crt;
+
+Procedure WriteArch;
+Procedure WritePopOS;
+
+implementation
+
+
+// Arch Linux Logo
+Procedure WriteArch;
+begin
+
+	TextColor(LightBlue);
+	writeLn('       /\\       ');
+	writeLn('      /  \\      ');
+	writeLn('     /\\  \\     ');
+	writeLn('    /      \\    ');
+	writeLn('   /   ,,   \\   ');
+	writeLn('  /   |  |  -\\  ');
+	writeLn(' /__-''    ''-__\\ ');
+	
+end;
+
+// PopOS Logo
+Procedure WritePopOS;
+begin
+
+	TextColor(White);
+	writeLn(' ______               ');
+	writeLn(' \   _ \        __    ');
+	writeLn('  \ \ \ \      / /    ');
+	writeLn('   \ \_\ \    / /     ');
+	writeLn('    \  ___\  /_/      ');
+	writeLn('     \ \    _         ');
+	writeLn('    __\_\__(_)_       ');
+	writeLn('   (___________)      ');
+	
+end;
+
+end.
