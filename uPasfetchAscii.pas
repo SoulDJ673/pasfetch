475581866bdfa0a457e83b95140ca46d62e63659
Drunken Alcoholic <drunkenalcoholic@protonmail.com>
2022-03-05 16:07:44 +0000

    Update uPasfetchAscii.pas

--- uPasfetchAscii.pas
+++ uPasfetchAscii.pas
@@ -1,42 +1,47 @@
 unit uPasfetchAscii;
-{$mode objfpc}{$H+}
+
+{$MODE OBJFPC}{$H+}{$J-}
 
 interface
 
-uses 
- Crt;
+uses
+  uAnsiCrt;
 
-Procedure WriteArch;
-Procedure WritePopOS;
+procedure WriteArch;
+procedure WritePopOS;
 
 implementation
 
+
 // Arch Linux Logo
-Procedure WriteArch;
+procedure WriteArch;
 begin
-	TextColor(LightBlue);
-	writeLn('       /\\       ');
-	writeLn('      /  \\      ');
-	writeLn('     /\\  \\     ');
-	writeLn('    /      \\    ');
-	writeLn('   /   ,,   \\   ');
-	writeLn('  /   |  |  -\\  ');
-	writeLn(' /__-''    ''-__\\ ');	
+
+  TextColor(LightBlue);
+  writeLn('       /\\       ');
+  writeLn('      /  \\      ');
+  writeLn('     /\\  \\     ');
+  writeLn('    /      \\    ');
+  writeLn('   /   ,,   \\   ');
+  writeLn('  /   |  |  -\\  ');
+  writeLn(' /__-''    ''-__\\ ');
+
 end;
 
-
 // PopOS Logo
-Procedure WritePopOS;
+procedure WritePopOS;
 begin
-	TextColor(White);
-	writeLn(' ______               ');
-	writeLn(' \   _ \        __    ');
-	writeLn('  \ \ \ \      / /    ');
-	writeLn('   \ \_\ \    / /     ');
-	writeLn('    \  ___\  /_/      ');
-	writeLn('     \ \    _         ');
-	writeLn('    __\_\__(_)_       ');
-	writeLn('   (___________)      ');	
+
+  TextColor(White);
+  writeLn(' ______               ');
+  writeLn(' \   _ \        __    ');
+  writeLn('  \ \ \ \      / /    ');
+  writeLn('   \ \_\ \    / /     ');
+  writeLn('    \  ___\  /_/      ');
+  writeLn('     \ \    _         ');
+  writeLn('    __\_\__(_)_       ');
+  writeLn('   (___________)      ');
+
 end;
 
 end.
